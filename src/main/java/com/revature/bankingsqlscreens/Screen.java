233c564bf0900dca0acf105215fccd90f287072c
kevingordon995 <kevingordon995@yahoo.com>
2018-08-20 23:55:11 -0400

    delete

--- src/main/java/com/revature/bankingsqlscreens/Screen.java
+++
@@ -1,7 +0,0 @@
-package com.revature.bankingsqlscreens;
-
-import com.revature.bankingsqlscreens.Screen;
-
-public interface Screen {
-	Screen start();
-}
--- src/main/java/com/revature/screens/CurrentUser.java
+++
@@ -1,7 +0,0 @@
-package com.revature.screens;
-import com.revature.user.User;
-
-public class CurrentUser {
-	public static User current;
-	
-}
